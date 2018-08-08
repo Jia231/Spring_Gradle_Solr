355ce21e262dcd861725e0139e9a9c51d5615dc3
jia <dliou471@gmail.com>
2018-08-07 23:11:36 -0600

    first commit

---
+++ src/main/java/com/javasampleapproach/solr/HelloController.java
@@ -0,0 +1,42 @@
+package com.javasampleapproach.solr;
+import java.util.Arrays;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+//newly added
+import org.springframework.beans.factory.annotation.Autowired;
+import com.javasampleapproach.solr.model.Customer;
+import com.javasampleapproach.solr.repo.CustomerRepository;
+
+@RestController
+public class HelloController {
+ @Autowired
+	private CustomerRepository customerRepository;
+    @RequestMapping("/")
+    public String index() {
+   String str = "";
+
+	customerRepository.deleteAll();
+
+		// Store customers
+		customerRepository.saveAll(Arrays.asList(new Customer("1", "Jack", 20), 
+											new Customer("2", "Adam", 24),
+											new Customer("3", "Kim", 27), 
+											new Customer("4", "David", 30), 
+											new Customer("5", "Peter", 21),
+											new Customer("6", "Jim", 21)));
+
+
+        str = "All customers: <br>" ; 
+		for (Customer product : this.customerRepository.findAll()) {
+			str = str + "<br>" + product.getName();
+		}
+
+        str = str + "<br><br>" + "Customers that name end in m" + "<br>";
+
+		for (Customer customer : this.customerRepository.findByNameEndsWith("m")) {
+            str = str + "<br>"  + customer.getName();
+		}
+        return str;
+    }
+
+}
---
+++ src/main/java/com/javasampleapproach/solr/SpringBootSolrApplication.java
@@ -0,0 +1,16 @@
+package com.javasampleapproach.solr;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSolrApplication implements CommandLineRunner {
+	@Override
+	public void run(String... args) throws Exception {
+    	System.out.println("Let's inspect the beans provided by Spring Boot:");	
+	}
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringBootSolrApplication.class, args);	
+	}
+}
