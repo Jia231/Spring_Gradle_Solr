084df759e52ae8fcd302bc5f4525c21c088fa31e
jia <dliou471@gmail.com>
2018-08-09 21:28:23 -0600

    post method added

--- src/main/java/com/javasampleapproach/solr/UserController.java
+++ src/main/java/com/javasampleapproach/solr/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.javasampleapproach.solr.model.Customer;
 import com.javasampleapproach.solr.repo.CustomerRepository;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 public class UserController {
@@ -17,6 +18,12 @@ public class UserController {
             customerList.add(product);
         }
         return customerList;
+    }
+    @PostMapping("/addUser")
+    public Customer addUser(Customer customer){
+        System.out.println(customer);
+        customerRepository.save(customer);
+        return customer;
     }
 }
 
