d99c5663ddca445768f9eb9b677aeed0ac21d269
jia <dliou471@gmail.com>
2018-08-11 20:42:53 -0600

    helper class and post route added

--- src/main/java/com/javasampleapproach/solr/UserController.java
+++ src/main/java/com/javasampleapproach/solr/UserController.java
@@ -6,11 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.javasampleapproach.solr.model.Customer;
 import com.javasampleapproach.solr.repo.CustomerRepository;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.javasampleapproach.solr.utils.JavaUtils;
 
 @RestController
 public class UserController {
     @Autowired
     private CustomerRepository customerRepository;
+    @Autowired
+    private JavaUtils javaUtils;
+
     @RequestMapping("/users")
     public ArrayList<Customer> users(){
         ArrayList<Customer> customerList = new ArrayList<>();
@@ -20,10 +26,16 @@ public class UserController {
         return customerList;
     }
     @PostMapping("/addUser")
-    public Customer addUser(Customer customer){
-        System.out.println(customer);
-        customerRepository.save(customer);
-        return customer;
+    public String addUser(@RequestBody Customer customer){
+       if(javaUtils.validCustomer(customer)){
+           int l = javaUtils.assignId();
+           l = l +1;
+           String id = Integer.toString(l);
+           Customer newCustomer = new Customer(id,customer.getName(),customer.getAge());
+           customerRepository.save(newCustomer);
+           return "new customer has been saved";
+       }
+          return "there was an error";
     }
 }
 
---
+++ src/main/java/com/javasampleapproach/solr/utils/JavaUtils.java
@@ -0,0 +1,34 @@
+package com.javasampleapproach.solr.utils;
+
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.javasampleapproach.solr.model.Customer;
+import com.javasampleapproach.solr.repo.CustomerRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JavaUtils{
+    @Autowired
+	private CustomerRepository customerRepository;
+    public boolean validCustomer(Customer newCustomer){
+
+        if(newCustomer.getName()==null || newCustomer.getName()=="" ){
+            return false;
+        }
+        else if(newCustomer.getAge()==null){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    public int assignId(){
+        System.out.println("in the assign class");
+        ArrayList<Customer> customerList = new ArrayList<>();
+        for(Customer product : this.customerRepository.findAll()){
+            customerList.add(product);
+        }
+        int l = customerList.size();
+        return l;
+    }
+}
