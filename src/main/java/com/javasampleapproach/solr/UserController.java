4fade70200356f6402b7ac8b29385b8c3b6b6e2e
jia <dliou471@gmail.com>
2018-08-12 22:26:23 -0600

    new changes

--- src/main/java/com/javasampleapproach/solr/UserController.java
+++ src/main/java/com/javasampleapproach/solr/UserController.java
@@ -7,7 +7,11 @@ import com.javasampleapproach.solr.model.Customer;
 import com.javasampleapproach.solr.repo.CustomerRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.javasampleapproach.solr.model.Response;
 import com.javasampleapproach.solr.utils.JavaUtils;
 
 @RestController
@@ -25,7 +29,7 @@ public class UserController {
         }
         return customerList;
     }
-    @PostMapping("/addUser")
+   /* @PostMapping("/addUser")
     public String addUser(@RequestBody Customer customer){
        if(javaUtils.validCustomer(customer)){
            int l = javaUtils.assignId();
@@ -33,9 +37,20 @@ public class UserController {
            String id = Integer.toString(l);
            Customer newCustomer = new Customer(id,customer.getName(),customer.getAge());
            customerRepository.save(newCustomer);
-           return "new customer has been saved";
+           ResponseEntity<String> responseEntity = new ResponseEntity<>("my response body",
+                                                                     HttpStatus.OK);
        }
           return "there was an error";
-    }
+    }*/
+    @PostMapping("/addUser")
+        public ResponseEntity<Response> addUser(RequestEntity<Customer> customer){
+            //System.out.println(customer.getBody().getName() + " " + customer.getBody().getAge());
+            Response res = new Response();
+            res.setData("User saved successfully");
+            ResponseEntity<Response> responseEntity = new ResponseEntity<>(res,
+                                                                     HttpStatus.BAD_REQUEST);
+            return responseEntity;
+        }
+    
 }
 
---
+++ src/main/java/com/javasampleapproach/solr/model/Response.java
@@ -0,0 +1,15 @@
+package com.javasampleapproach.solr.model;
+
+public class Response{
+
+    private String data;
+
+    public void setData (String data){
+        this.data = data;
+    }
+
+    public String getData(){
+        return data;
+    }
+
+}
